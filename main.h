a15920de27702711d95dc5a85dfd3c6413ee6a31
Dave McGregor <davejmcg@gmail.com>
2013-01-28 20:00:21 -0500

    updated

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN
 #define MAIN
-
+#include <iostream>
 int errorCheck();
 void admin(GtkWidget*, gpointer);
 void callback();
-void application(GtkWidget*,gpointer);
+void application(GtkWidget*, GtkWidget*);
 int main(int,char**);
 #endif
