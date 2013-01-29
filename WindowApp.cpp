5e5bd9479374c8eb03f3500a06caf1c16eb7f1cf
Dave McGregor <davejmcg@gmail.com>
2013-01-29 11:20:18 -0500

    Button now resizes the window, adds a text box. All data is encapsulated within the WindowApp file. templates for getters and setters are there as well

---
+++ WindowApp.cpp
@@ -0,0 +1,38 @@
+#include "WindowApp.h"
+
+WindowApp::WindowApp(){
+
+
+}
+
+GtkWidget* WindowApp::getLabel(){
+
+}
+GtkWidget* WindowApp::getWindow(){
+
+}
+GtkWidget* WindowApp::getFrame(){
+
+}
+GtkWidget* WindowApp::getApply(){
+
+}
+GtkWidget* WindowApp::getLogin(){
+
+}
+
+void WindowApp::setLabel(){
+
+}
+void WindowApp::setWindow(){
+
+}
+void WindowApp::setFrame(){
+
+}
+void WindowApp::setApply(){
+
+}
+void WindowApp::setLogin(){
+
+}
---
+++ WindowApp.h
@@ -0,0 +1,24 @@
+#ifndef WINDOWAPP_H
+#define WINDOWAPP_H
+#include <iostream>
+#include <gtk/gtk.h>
+using namespace std;
+class WindowApp{
+	public:
+		GtkWidget *label, *window, *frame, *apply, *login;
+		WindowApp();
+	private:
+		GtkWidget* getLabel();
+		GtkWidget* getWindow();
+		GtkWidget* getFrame();
+		GtkWidget* getApply();
+		GtkWidget* getLogin();
+
+		void setLabel();
+		void setWindow();
+		void setFrame();
+		void setApply();
+		void setLogin();		
+};
+
+#endif
